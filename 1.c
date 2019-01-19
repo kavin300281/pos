d62aa26d335925e9e0f5e9787a95c7cf59f46b63
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-01-19 14:42:39 +0530

    1.c

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    printf("enter the test case");
+    scanf("%d",&t);
+    {
+int a;
+scanf("%d",&a);
+if(a>0)
+printf("positive");
+if(a==0)
+printf("zero");
+if(a<0)
+printf("negative");
+}
+    return 0;
+}
