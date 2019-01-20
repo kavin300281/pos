a80fee99edd918811063c1d85a934c9920049a4b
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-01-20 06:50:41 -0800

    Create power.c

---
+++ power.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c=1,i;
+    scanf("%d%d",&a,&b);
+    for(i=0;i<b;i++)
+    {
+     c=c*a;   
+        
+    }
+    printf("%d",c);
+    
+
+
+
+    return 0;
+}
