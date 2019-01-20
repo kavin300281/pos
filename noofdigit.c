854e82280494ed412362bf5cde673535839e27a1
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-01-20 06:44:27 -0800

    Create noofdigit.c

---
+++ noofdigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,l=0;
+    scanf("%d",&a);
+    while(a)
+    {
+        l++;
+        a=a/10;
+        
+    }
+    printf("%d",l);
+    
+
+
+
+    return 0;
+}
