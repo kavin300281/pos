2c55e0fbc41385858c395bc200c51bd2073d5d61
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-01-28 09:29:38 -0800

    Create print no of string.c

---
+++ print no of string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    char b[100];
+    scanf("%s",b);
+    scanf("%d",&a);
+     while(a)
+     {
+         printf("%s\n",b);
+         a--;
+     }
+     
+    return 0;
