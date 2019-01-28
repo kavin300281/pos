81fc6e3b57c644ebee7252a460dc5981fa9f83b3
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-01-28 07:51:02 -0800

    Create sub.c

---
+++ sub.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e,f,sub,rem,q;
+scanf("%d %d",&a,&b);
+scanf("%d %d",&c,&d);
+e=(60*a)+b;
+f=(60*c)+d;
+sub=e-f;
+q=sub/60;
+rem=sub%60;
+printf("%d %d",q,rem);
+getch();
+
+}
