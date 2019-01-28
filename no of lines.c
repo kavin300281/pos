67b4c73ada97fdde318dbdb008d45d666739f72b
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-01-28 08:17:52 -0800

    Create no of lines.c

---
+++ no of lines.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char a[100];
+    int i,n=1;
+    scanf("%[^\n]s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]=='.')
+        {
+        n++;
+        }
+        
+    }
+printf("%d",n);
+    return 0;
+}
