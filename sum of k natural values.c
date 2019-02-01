06bdfb47dcaa77196755ca58e95da97a9e95e883
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-02-01 07:12:59 -0800

    Create sum of k natural values.c

---
+++ sum of k natural values.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   long int m,l=0,i;
+    scanf("%ld",&m);
+    for(i=1;i<=m;i++)
+    {
+        
+        l=l+i;
+        
+    }
+    printf("%d",l);
+    return 0;
+
+}
