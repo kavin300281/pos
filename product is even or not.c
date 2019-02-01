b41826313319f849a3b60e06915f5564a1e9cc1c
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-02-01 06:21:33 -0800

    Create product is even or not.c

---
+++ product is even or not.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,m;
+    scanf("%d%d",&n,&m);
+    n=n*m;
+    if(n%2==0)
+    {
+        printf("even");
+    }
+    else
+    printf("odd");
+    
+
+    return 0;
+}
