35ebc2562d416964be7df3ad0a669cdb6af3a9d7
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-02-06 06:23:13 -0800

    Create multiply13.c

---
+++ multiply13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int b,l=0,i;
+    scanf("%d",&b);
+    
+        if(b%13==0)
+        {
+            printf("yes");
+        }
+    else
+    printf("no");
+
+
+    return 0;
+}
