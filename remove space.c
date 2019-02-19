80bb3f5df71b91485abc32b71922146255bc0d87
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-02-19 08:02:17 -0800

    Create remove space.c

---
+++ remove space.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+ int n=0,i,c=0;
+ char a[100],b[100];
+ scanf("%[^\n]s",&a);
+ for(i=0;a[i]!='\0';i++)
+ {
+     if(a[i]==' ')
+     {
+         
+     }
+     else
+     {
+         b[c]=a[i];
+         c++;
+     }
+ }
+ for(i=0;i<c;i++)
+ {
+     printf("%c",b[i]);
+ }
+return 0;
+}
