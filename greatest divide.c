910ed1ec1f3ef2f5fe161b0d6123ce979959b64e
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-02-19 06:58:35 -0800

    Create greatest divide.c

---
+++ greatest divide.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+ int a,b,den,num,rem,c;
+ scanf("%d%d",&a,&b);
+ if(a>b)
+ {
+     num=a;
+     den=b;
+ }
+ else
+ {
+     num=b;
+     den=a;
+ }
+ rem=num%den;
+ while(rem)
+ {
+     num=den;
+     den=rem;
+     rem=num%den;
+ }
+ c=den;
+printf("%d",c);
+return 0;
+}
