b32c34a16716ef5ac25f3af9acc10570380800f4
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-03 08:47:18 -0800

    Create ll.c

---
+++ ll.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main()
+{
+ int n,a[100],b[100],l=0,m=0,c,i,j;
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+     scanf("%d",&a[i]);
+ }
+ for(i=0;i<n;i++)
+ {
+     l=0;
+     for(j=0;j<n;j++)
+     {
+         if(a[i]==a[j])
+         {
+            l++; 
+         }
+         
+     }
+     b[m]=l;
+     m++;
+}
+c=0;
+ for(i=0;i<m;i++)
+ {
+     if(c<b[i])
+     {
+         c=b[i];
+     }
+ }
+  printf("%d",c);
+    return 0;
+}
+#include <stdio.h>
+
+int main()
+{
+ int n,a[100],b[100],l=0,m=0,c,i,j;
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+     scanf("%d",&a[i]);
+ }
+ for(i=0;i<n;i++)
+ {
+     l=0;
+     for(j=0;j<n;j++)
+     {
+         if(a[i]==a[j])
+         {
+            l++; 
+         }
+         
+     }
+     b[m]=l;
+     m++;
+}
+c=0;
+ for(i=0;i<m;i++)
+ {
+     if(c<b[i])
+     {
+         c=b[i];
+     }
+ }
+  printf("%d",c);
+    return 0;
+}
