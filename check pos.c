28e657fe38a5b368f0f70c2f3ac582ef03e06573
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-03 05:32:48 -0800

    Create check pos.c

---
+++ check pos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[100],b[1];
+    int i,n;
+    scanf("%s %s",&a,&b);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(b[0]==a[i])
+        printf("%d",i+1);
+    }
+
+    return 0;
+}
