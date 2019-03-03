05e4e549139749782e3b65c04a477bf2ef6206c9
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-03 08:44:43 -0800

    Update longest length.c

--- longest length.c
+++ longest length.c
@@ -2,35 +2,20 @@
 
 int main()
 {
- int n,a[100],b[100],l=0,m=0,c,i,j;
- scanf("%d",&n);
+ int n,a[100],b[100],l=0,m=0,c,i,j,k;
+ scanf("%d %d",&n,&k);
  for(i=0;i<n;i++)
  {
      scanf("%d",&a[i]);
  }
- for(i=0;i<n;i++)
+ 
+ for(i=0;i<n-k;i++)
  {
-     l=0;
-     for(j=0;j<n;j++)
-     {
-         if(a[i]==a[j])
-         {
-            l++; 
-         }
-         
-     }
-     b[m]=l;
-     m++;
-}
-c=0;
- for(i=0;i<m;i++)
- {
-     if(c<b[i])
-     {
-         c=b[i];
-     }
+     printf("%d ",a[i]);
  }
-  printf("%d",c);
+  
     return 0;
 }
+
+
 
