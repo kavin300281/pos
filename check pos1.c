e6f2cea882ad996971d351f25f0464f2e93650df
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-04 07:36:01 -0800

    Create check pos1.c

---
+++ check pos1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+ int a[100],n,i,k;
+ scanf("%d %d",&n,&k);
+ for(i=0;i<n;i++)
+ {
+     scanf("%d",&a[i]);
+     
+ }
+ for(i=0;i<n;i++)
+ {
+     if(a[i]==k)
+     {
+         printf("%d",i+1);
+     }
+ }
+ 
+
+    return 0;
+}
