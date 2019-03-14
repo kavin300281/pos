6d0bf09c1240d2d312a21c918799525a219fcbd1
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-14 08:27:45 -0700

    Create bitwise AND array.c

---
+++ bitwise AND array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100],i,j,n,l,max=-1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        max=max&a[i];
+    }
+        
+printf("%d",max);
+    return 0;
+}
