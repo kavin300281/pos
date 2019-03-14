7e5270aa244ad7519bb0e34334667d7daf267f27
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-14 08:59:25 -0700

    Create left shift.c

---
+++ left shift.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,k,i,sum;
+    scanf("%d%d",&n,&k);
+    
+    
+        
+            i=n<<k;
+        
+    
+    
+        
+printf("%ld",i);
+    return 0;
+}
