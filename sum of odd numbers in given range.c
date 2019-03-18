777d6491d983252f80d80ebea4813422b6d3c89e
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-18 07:44:36 -0700

    Create sum of odd numbers in given range.c

---
+++ sum of odd numbers in given range.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+int a,b,sum=0,i;
+scanf("%d%d",&a,&b);
+
+for(i=a;i<b;i++)
+{
+    if(i%2!=0)
+    {
+        sum=sum+i;
+    }
+    
+}
+ printf("%d",sum);
+    return 0;
+}
