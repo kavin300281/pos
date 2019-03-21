bf26315ee28c13c5f8f32a0fcaf42df097587cd1
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-21 06:54:04 -0700

    Update no of subarray.c

--- no of subarray.c
+++ no of subarray.c
@@ -1,1 +1,18 @@
 
+#include <stdio.h>
+
+int main()
+{
+int a[10],n,i,s=0;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+for(i=1;i<=n;i++)
+{
+    s=s+i;
+}
+printf("%d",s);
+    return 0;
+}
