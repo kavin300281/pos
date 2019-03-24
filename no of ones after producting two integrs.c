1ec742ec760079a572234119b684b3c6bae8f2f0
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-24 17:34:44 +0530

    Create no of ones after producting two integrs.c

---
+++ no of ones after producting two integrs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,i=0,rem,s=0,ba=1,r;
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    while(c)
+    {
+        rem=c%2;
+        s=s+(rem*ba);
+        ba=ba*10;
+        c=c/2;
+        if(rem==1)
+        {
+            i++;
+        }
+        
+        
+    }
+
+        printf("%d",i);
+        
+    
+
+    return 0;
+}
