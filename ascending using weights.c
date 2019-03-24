592c93caab55b0230f08a6283992b45afbd15d6c
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-24 10:22:40 +0530

    Create ascending using weights.c

---
+++ ascending using weights.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100],b[100],i,j,n,temp;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    scanf("%d",&b[i]);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(b[i]>b[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+
+    return 0;
+}
