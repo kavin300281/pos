25327a2087099970917a90a707e5d30b09715066
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-24 17:29:56 +0530

    Create right left first one.c

---
+++ right left first one.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,i=1,rem,s=0,ba=1,r;
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    while(c)
+    {
+        rem=c%2;
+        s=s+(rem*ba);
+        ba=ba*10;
+        c=c/2;
+        
+        
+    }
+while(i)
+{
+    s=s/10;
+    r=s%10;
+    i++;
+    if(r==1)
+    {
+        printf("%d",i);
+        break;
+    }
+    
+}
+    return 0;
+}
