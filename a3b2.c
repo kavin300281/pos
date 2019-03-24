5e9d109e3f9becb68d31687c7c4323d7d7aaf111
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-03-24 19:30:44 +0530

    Update a3b2.c

--- a3b2.c
+++ a3b2.c
@@ -3,26 +3,23 @@
 int main()
 {
     char a[100];
-    int i,j,k,l=0;
-    
-    scanf("%[^\n]s",&a);
+  int i,j,k,c=0,l=0,n=0;
+    scanf("%s",a);
     for(i=0;a[i]!='\0';i++)
     {
-        l++;
+        n++;
     }
-    
-    for(i=0;i<l;i++)
+    for(i=0;i<n;i++)
     {
-        if(a[i+1]>=47&&a[i+1]<=56)
+        c=0;
+        for(j=0;j<n;j++)
         {
-            k=a[i+1]-48;
-            for(j=0;j<k;j++)
-            {
-                printf("%c",a[i]);
-            }
+         if(a[i]==a[j])
+         c++;
         }
+        if(a[i]!=a[i-1])
+        printf("%c%d",a[i],c);
     }
-    
 
     return 0;
 }
