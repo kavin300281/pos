a43e07b730a0260622f55b5f92be244e5b1d6cbf
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-04-03 15:46:52 +0530

    Create hunter4.c

---
+++ hunter4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100],i,j,c=0,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+for(i=0;i<n;i++)
+{
+    c=0;
+    for(j=0;j<n;j++)
+    {
+       if(a[i]==a[j])
+       {
+           c++;
+       }
+    }
+    if(c==1)
+    printf("%d",a[i]);
+
+}
+    return 0;
+}
