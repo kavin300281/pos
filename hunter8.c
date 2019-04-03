9a841d9fc82485abe607ce4940e55d348c0d1954
kavin300281 <46838484+kavin300281@users.noreply.github.com>
2019-04-03 16:27:43 +0530

    Create hunter8.c

---
+++ hunter8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100],b[100],i,j,c=0,n,m,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+    for(k=j+1;k<n;k++)
+    {
+        if(a[i]+a[j]==a[k])
+        printf("%d %d %d \n",a[i],a[j],a[k]);
+    }
+    
+}
+}
+
+    return 0;
+}
